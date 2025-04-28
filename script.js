5fc3b1c100e136c5b48d5854dd325d3e9c4d0acf
pratiush pathak <pratiushpathak@gmail.com>
2025-04-28 17:36:55 +0530

    added local storage project completed

--- script.js
+++ script.js
@@ -5,20 +5,71 @@ const count=document.getElementById('count');
 const total=document.getElementById('total');
 const movieSelect=document.getElementById('movie');
 let ticketPrice=+movieSelect.value;//+ convert string into number 
-console.log(ticketPrice);
+//console.log(ticketPrice);
+//Save selected movie index and price
+populateUi()
+
+function setMovieData(movieIndex,moviePrice)
+{
+  localStorage.setItem('selectedMovieIndex',movieIndex);
+  localStorage.setItem('selectedMoviePrice',moviePrice)
+
+}
+
 //update total count
 function updateSelectedCount()
 {
   const selectedSeats= document.querySelectorAll('.row .seat.selected');
   //console.log(selectedSeats)
+
+  // saving to local storage
+  //copy selectd seats into arr
+  // map through array
+  // return a new array indexes 
+  //spread operator convert node list in array in our case->
+  const seatsIndex=[...selectedSeats].map(function(seat){
+    return[...seats].indexOf(seat)
+    
+
+  });
+  localStorage.setItem('selectedseats',JSON.stringify(seatsIndex)) 
+//code end local storage
   const selectedSeatsCount=selectedSeats.length;
   count.innerText=selectedSeatsCount;
   total.innerText=selectedSeatsCount*ticketPrice;
    
 }
-//movie slect event
+
+
+//get data from local storage and populate ui 
+function populateUi(){
+
+const selectedSeats=JSON.parse(localStorage.getItem('selectedseats'));
+//console.log(selectedSeats);
+if(selectedSeats!==null && selectedSeats.length>0)
+{
+  seats.forEach((seat,index)=>{
+if(selectedSeats.indexOf(index)>-1)
+{
+  seat.classList.add('selected');
+
+}
+  })
+}
+const selectedMovieIndex=localStorage.getItem('selectedMovieIndex');
+if(selectedMovieIndex!==null)
+{
+  movieSelect.selectedIndex=selectedMovieIndex;
+}
+
+
+}
+
+
+//movie select event
 movieSelect.addEventListener('change',(e)=>{
   ticketPrice= +e.target.value;
+  setMovieData(e.target.selectedIndex,e.target.value);
   updateSelectedCount();
 
 })
@@ -34,3 +85,5 @@ container.addEventListener('click',(e)=>{
   }
 
 })
+//initial count and total set
+updateSelectedCount();
